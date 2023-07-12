edda26f3bea0e6ee2f2bf731c18e3dd592b781e2
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-07-12 22:04:38 +0530

    intial commit

---
+++ src/TapInstacks/Strings/StringSentence/NextInputSentence.java
@@ -0,0 +1,15 @@
+package TapInstacks.Strings.StringSentence;
+
+import java.util.Scanner;
+
+public class NextInputSentence {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] str = new String[n];
+        for(int i = 0;i<str.length;i++){
+            str[i] = scan.next();
+        }
+    }
+}
